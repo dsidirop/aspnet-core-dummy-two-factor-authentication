07a5ba0c3052f2167fa0d211d0abad418347c558
Dominic Sidiropoulos <dominick.sidiropoulos@gmail.com>
2021-03-28 01:52:19 +0200

    Add a gif animation on the well-done page

---
+++ Code/Web/TwoFactorAuth.Web/wwwroot/js/welldone/welldone-view-controller.js
@@ -0,0 +1,55 @@
+﻿; //vital   keep this
+
+(function (self, $, _window, _document, _gifffer) {
+
+    "use strict";
+
+    let _config = {
+        rootContainerSelector: ""
+    };
+
+    let _$els = {
+        $window: null,
+        $document: null,
+        $rootContainer: null
+    };
+
+    self.Init = function (config) {
+        try {
+            initConfig(config);
+            hookElements();
+            initImplementation();
+        } catch (err) {
+            console.error("[WDC.INIT010] [BUG] Initialization failed", err);
+        }
+    };
+
+    function hookElements() {
+        _$els.$window = $(_window);
+        _$els.$document = $(_document);
+        _$els.$rootContainer = _$els.$document.find(_config.rootContainerSelector);
+    }
+
+    function initConfig(config) {
+        _config = config;
+    }
+
+    function initImplementation() {
+        const gifs = _gifffer({ //todo  fork gifffer and sanitize it to make its api more appropriate
+            playButtonStyles: {'display': 'none'},
+            playButtonIconStyles: {'display': 'none'}
+        });
+
+        const handler = function() {
+            if (gifs.length === 0) {
+                _window.setTimeout( handler, 5);
+                return;
+            }
+
+            $(gifs).click(); //start animation
+        };
+
+        _window.setTimeout( handler, 50);
+    }
+
+})((window.WelldoneController = window.WelldoneController || {}), window.jQuery, window, document, window.Gifffer);
